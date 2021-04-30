96438c3d32fc921618712b4eeecb3e4c06c0d11a
hoi412 <58466125+hoi412@users.noreply.github.com>
2021-04-30 21:05:00 +0200

    Create PauseMenu.cs

---
+++ PauseMenu.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+	public static bool GameIsPaused = false;
+
+	public GameObject pauseMenuUI;
+	
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			if (GameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
+
+	public void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+		Debug.Log ('R');
+	}
+
+	void Pause()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	public void LoadMenu()
+	{
+		Time.timeScale = 1f;
+		Debug.Log("Loading Menu...");
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void QuitGame()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		Debug.Log("Quitting game...");
+		
+	}
+
+	
+
+
+}
